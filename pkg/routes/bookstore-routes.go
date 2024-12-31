127980037e9699b0f28c9059a8180178895e7bcf
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-12-31 17:46:43 +0530

    CRUD: update function left

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
-	"github/gorilla/mux"
+	"github.com/BhuwanSingh/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
 )
 
 var RegisterBookstore = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.createBook).Methods
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookbyID).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
